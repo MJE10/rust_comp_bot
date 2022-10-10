1748b64791e66edc6251e9927d5ec502b9569b09
MJE10 <63426573+MJE10@users.noreply.github.com>
2022-10-09 21:50:48 -0400

    Add Rocket with Hello World example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> T {
+    rocket::build().mount("/", routes![index])
 }
