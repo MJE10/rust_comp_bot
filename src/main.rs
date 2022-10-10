ed99b889e1e84d851228aec96df5a15e8776e6b6
MJE10 <63426573+MJE10@users.noreply.github.com>
2022-10-09 22:09:17 -0400

    Fix errors with generic typing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[macro_use] extern crate rocket;
 
 use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
 
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> Rocket<Build> {
     rocket::build().mount("/", FileServer::from("static"))
 }
